f0a3c2a9210d7be8e6458ad012341cd8031bede9
parthpachchigar <parthpachchigar@gmail.com>
2017-11-19 00:05:04 -0800

    Added TODOs for Dhanashree

--- netty-pipe2/src/gash/router/server/edge/EdgeMonitor.java
+++ netty-pipe2/src/gash/router/server/edge/EdgeMonitor.java
@@ -15,4 +15,19 @@ public class EdgeMonitor {
 		}
 		return instance;
 	}
+	public static void discoverEdges() {
+		//TODO: Write the same code as given in the main method of UdpClient
+		//TODO: After writing that code here change handler from UdpClientHandler to EdgeDiscoveryHandler over here
+	}
+	public static void displayEdgeStatus() {
+		// gash.router.server.edge.EdgeInfo.availableNodes contains the id of nodes which are active
+		// TODO: from gash.router.server.state.State.myConfig for each nodeId available in RoutingEntry 
+		// array list check if node id is present in gash.router.server.edge.EdgeInfo.availableNodes
+		// and if present print that node as active otherwise inactive
+	}
+	
+	// TODO: Create two timer tasks
+	// One will call discoverEdges every 5 sec
+	// Another will call displayEdgeStatus every 4 sec 
+	
 }
