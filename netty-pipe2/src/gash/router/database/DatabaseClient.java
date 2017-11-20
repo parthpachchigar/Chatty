313cdc428d3d50e9b4868334a82721618ee27cbb
dgaonkarf17 <dgaonkar17@gmail.com>
2017-11-20 10:40:58 -0800

    Database module commit

---
+++ netty-pipe2/src/gash/router/database/DatabaseClient.java
@@ -0,0 +1,31 @@
+package gash.router.database;
+
+import java.sql.ResultSetMetaData;
+import java.util.List;
+
+public interface DatabaseClient {
+	
+	byte[] get(String key);
+	
+	String post(byte[] image, long timestamp);
+	
+	public void put(String key, byte[] image, long timestamp);
+	
+	public void delete(String key);
+
+	long getCurrentTimeStamp();
+
+	List<Record> getNewEntries(long staleTimestamp);
+
+	void putEntries(List<Record> list);
+
+	List<Record> getAllEntries();
+
+	void post(String key, byte[] image, long timestamp);
+
+	ResultSetMetaData getMessage(String key);
+	void postMessage(String message, String toId,String fromId);
+
+	void createDb();
+	
+}
